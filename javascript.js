fd884e2154dbe87d3a7b447b43f164d93c6156ea
Kevin Le <kevinle29299@gmail.com>
2025-01-20 11:35:00 -0800

    Implement the main mechanics of the game.
    Wrote the function for computer choice using
    Math.random() function.
    
    Wrote the function for human choice that prompt
    users to enter a string value.
    
    Wrote the function playRound which compares both
    choices and decides on a winner.
    
    Wrote the function playGame which simulates 5
    games, increment each players' scores, and
    announces the winner at the end.
    
    The game is played through the Google's console.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,65 @@
-// console.log("Hello World");
+function playGame()
+{
+    let humanScore = 0;
+    let computerScore = 0;
+    function playRound(human, computer)
+    {
+        if(human == "Rock" && computer == "Scissors" ||
+            human == "Paper" && computer == "Rock" || 
+            human == "Scissors" && computer == "Paper")
+        {
+            humanScore++;
+            alert(`Human wins! ${human} beats ${computer}`);
+        } 
+        else if (computer == "Rock" && human == "Scissors" || 
+            computer == "Paper" && human == "Rock" ||
+            computer == "Scissors" && human == "Paper")
+        {
+            computerScore++;
+            alert(`Computer wins! ${computer} beats ${human}`);
+        }
+        else
+            alert("Tie!")
+        return;
+    }
 
-//Computer Choice
+    for(let i = 0; i < 5; i++)
+    {
+        let computerSelection = getComputerChoice();
+        let humanSelection = getHumanChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    if(humanScore > computerScore)
+        return `Human wins! ${humanScore} - ${computerScore}`;
+    else if(humanScore < computerScore)
+        return `Computer wins! ${computerScore} - ${humanScore}`;
+    else
+        return `Nobody wins! ${humanScore} - ${computerScore}`
+}
+
 function getComputerChoice()
 {
     let temp = Math.random();
     if(temp < 0.33)
-        console.log("Rock");
+        return "Rock";
     else if(temp >= 0.33 && temp < 0.66)
-        console.log("Paper");
+        return "Paper";
     else if(temp >= 0.66)
-        console.log("Scissors");
+        return "Scissors";
 }
 
-getComputerChoice();
+function getHumanChoice()
+{
+    let choice = prompt("Pick one: Rock, Paper, Scissors");
+    choice = choice.charAt(0).toUpperCase() + choice.substring(1).toLowerCase()
+
+    if(choice == "Rock" || choice == "Paper" || choice == "Scissors")
+        return choice;
+    else    
+        alert("Invalid");
+}
+
+
+// Start the game
+alert(playGame());
